61c5860cbcac0e724e1eb5c476bec0bc921e1de3
Angela Liu <angelaliu@Angelas-MacBook-Air.local>
2023-06-14 12:00:45 +1200

    Sign up page and form created

--- server.py
+++ server.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
+    temp_form_data={
+        "firstname" : "James",
+        "surname" : "Lovelock",
+        "email" : "jlovelock@gmail.com",
+        "aboutme" : "I love playing badminton with friends <3"
+    }
     return render_template("signup.html")
 
 if __name__ == "__main__":
